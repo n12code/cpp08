7411f9d2fee33e1b6c734eb48d08103ccbe6fa7a
nathan bodin <nathanbodin006@gmail.com>
2026-03-11 19:22:23 +0100

    started ex02 very briefly

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbodin <nbodin@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/11 19:03:26 by nbodin            #+#    #+#             */
+/*   Updated: 2026/03/11 19:21:42 by nbodin           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+
+//implement a mutantstack class that inherits from the stack class
+//call its constructors and its operatora for the canonical form
+//implement all types of iterators and all types of functions for them, const and non const
+//use template so that it suits every type for the stack
+
+int main(void)
+{
+    return (0);
+}
